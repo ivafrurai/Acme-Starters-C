0d795ec99d8fb9459eb5fa14b68d4a64fa48f9bc
Ivan Frutos <ivafrurai@alum.us.es>
2026-02-23 16:12:44 +0100

    Strategy,Tactic y Validators acabados

--- src/main/java/acme/entities/strategies/Strategy.java
+++ src/main/java/acme/entities/strategies/Strategy.java
@@ -1,9 +1,15 @@
 
 package acme.entities.strategies;
 
+import java.time.temporal.ChronoUnit;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
 import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Moment;
@@ -12,9 +18,11 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoment.Constraint;
 import acme.client.components.validation.ValidUrl;
+import acme.client.helpers.MomentHelper;
 import acme.constraints.ValidHeader;
 import acme.constraints.ValidText;
 import acme.constraints.ValidTicker;
+import acme.realms.Fundraiser;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -57,17 +65,41 @@ public class Strategy extends AbstractEntity {
 	private String				moreInfo;
 
 	@Mandatory
-	@Valid
+	//@Valid
 	@Column
-	private Boolean				draftMode;
+	private boolean				draftMode;
 
 	// Derived attributes -----------------------------------------------------
 
-	//aqui va el repositorio
+	@Transient
+	@Autowired
+	private StrategyRepository	repository;
 
-	//public Double monthsActive
-	//public Double expectedPercentage
+
+	@Valid
+	@Transient
+	public Double monthsActive() {
+		return (double) MomentHelper.computeDuration(this.startMoment, this.endMoment).get(ChronoUnit.MONTHS);
+	}
+
+	@Valid
+	@Transient
+	public Double expectedPercentage() {
+		Double result;
+		Double expectedPercentage;
+
+		expectedPercentage = this.repository.expectedPercentage(this.getId());
+		result = expectedPercentage == null ? 0 : expectedPercentage.doubleValue();
+
+		return result;
+	}
 
 	// Relationships ----------------------------------------------------------
+
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Fundraiser fundraiser;
 
 }
