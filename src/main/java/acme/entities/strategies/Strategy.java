5c72dde01a6c9e8454cf5e5c3d0ae68143884599
Ivan Frutos <ivafrurai@alum.us.es>
2026-02-24 11:44:05 +0100

    Strategy Sample Data finished

--- src/main/java/acme/entities/strategies/Strategy.java
+++ src/main/java/acme/entities/strategies/Strategy.java
@@ -2,17 +2,19 @@
 package acme.entities.strategies;
 
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import acme.client.components.basis.AbstractEntity;
-import acme.client.components.datatypes.Moment;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
@@ -20,6 +22,7 @@ import acme.client.components.validation.ValidMoment.Constraint;
 import acme.client.components.validation.ValidUrl;
 import acme.client.helpers.MomentHelper;
 import acme.constraints.ValidHeader;
+import acme.constraints.ValidStrategy;
 import acme.constraints.ValidText;
 import acme.constraints.ValidTicker;
 import acme.realms.Fundraiser;
@@ -29,7 +32,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-//@ValidStrategy
+@ValidStrategy
 public class Strategy extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
@@ -50,15 +53,17 @@ public class Strategy extends AbstractEntity {
 	@Mandatory
 	@ValidText
 	@Column
-	private String				descripition;
+	private String				description;
 
 	@Mandatory
 	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
-	private Moment				startMoment;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				startMoment;
 
 	@Mandatory
 	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
-	private Moment				endMoment;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				endMoment;
 
 	@Optional
 	@ValidUrl
