b77524ef1faae30227431c9b1bdcb8873266a9f3
alvgarlop <alvgarlop@alum.us.es>
2026-03-09 16:14:29 +0100

    Correccion validator

--- src/main/java/acme/constraints/SponsorshipValidator.java
+++ src/main/java/acme/constraints/SponsorshipValidator.java
@@ -6,9 +6,11 @@ import javax.validation.ConstraintValidatorContext;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import acme.client.components.validation.AbstractValidator;
+import acme.client.components.validation.Validator;
 import acme.entities.sponsorships.Sponsorship;
 import acme.entities.sponsorships.SponsorshipRepository;
 
+@Validator
 public class SponsorshipValidator extends AbstractValidator<ValidSponsorship, Sponsorship> {
 
 	// Internal state ---------------------------------------------------------
